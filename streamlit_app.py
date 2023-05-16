709415a7a197fcaf06267255f2f7f416797fd463
cchaitanya617 <133822263+cchaitanya617@users.noreply.github.com>
2023-05-16 19:57:05 -0300

    Update streamlit_app.py
    added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title('🥣Success is yours')
-streamlit.header('🥗hardwork matters')
-streamlit.text('🐔you can do it')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
