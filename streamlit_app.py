0f152affd82b180a06b6835f7ad4746fc91fabe3
cchaitanya617 <133822263+cchaitanya617@users.noreply.github.com>
2023-05-16 19:56:16 -0300

    Update streamlit_app.py
    added breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('🥣Success is yours')
 streamlit.header('🥗hardwork matters')
 streamlit.text('🐔you can do it')
-  
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
